59ed73e682d1ccb46620aaab92c9961bc3e95b72
LeoCo <leonardo.cotti@gmail.com>
2017-03-08 00:36:14 +0100

    Commit

---
+++ load_txt_data.py
@@ -0,0 +1,48 @@
+import re
+
+class load_txt_data(object):
+
+    def __init__(self,filename):
+        self.filename = filename
+
+    def clean_string(self,x):
+        if x == 'None':
+            return None
+        else:
+            return x[1:-1]
+
+    def get_data(self):
+
+        with open(self.filename) as f:
+            content = f.readlines()
+        content = [x.strip() for x in content]
+
+        #Clean firsts rows and last one
+        del content[0:4]
+        content.pop()
+
+        #Recreate the states variable
+        states = {}
+
+        for i in xrange(0, len(content),6):
+
+            regular_exp = re.match(r'\((.*),\s(.*),\s(.*),\s(.*),\s(.*)\)', content[i])
+
+            key = (self.clean_string(regular_exp.group(1)),
+                   self.clean_string(regular_exp.group(2)),
+                   self.clean_string(regular_exp.group(3)),
+                   self.clean_string(regular_exp.group(4)),
+                   self.clean_string(regular_exp.group(5)))
+
+            value = { None : float(re.match(r'--.*: (.*)', content[i + 1]).group(1)),
+                      'forward' : float(re.match(r'--.*: (.*)', content[i + 2]).group(1)),
+                      'right' : float(re.match(r'--.*: (.*)', content[i + 3]).group(1)),
+                      'left' : float(re.match(r'--.*: (.*)', content[i + 4]).group(1))}
+
+            states[key] = value
+
+        return states
+
+if __name__ == '__main__':
+
+    print load_txt_data("logs/sim_improved-learning.txt").get_data()
