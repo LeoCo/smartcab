59f4378e29b9c0dfb80322fa8d1eacbaccff153f
LeoCo <leonardo.cotti@gmail.com>
2017-03-07 02:12:21 +0100

    Commit

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -57,6 +57,12 @@ class LearningAgent(Agent):
         ###########
         # Set 'state' as a tuple of relevant data for the agent        
         state = None
+        print "Waypoint:"
+        print waypoint
+        print "Inputs"
+        print inputs
+        print "Deadline"
+        print deadline
 
         return state
 
@@ -172,7 +178,7 @@ def run():
     #   display      - set to False to disable the GUI if PyGame is enabled
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator(env, update_delay=0.01, log_metrics=True)
+    sim = Simulator(env, update_delay=2, log_metrics=True)
     
     ##############
     # Run the simulator
